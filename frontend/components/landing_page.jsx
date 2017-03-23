4018b115daeb062345f411db23e9e449427557f0
Claire Wild <claire.wild15@gmail.com>
2017-03-23 10:17:14 -0700

    Finish landing page except for logo

--- frontend/components/landing_page.jsx
+++ frontend/components/landing_page.jsx
@@ -5,8 +5,6 @@ const LandingPage = () => (
   <div>
     <div className="landing-header">
 
-
-
       <h1>NEVERNOTE</h1>
       <nav className="landing-nav">
         <Link to="/login"><p>Sign In</p></Link>
@@ -15,15 +13,24 @@ const LandingPage = () => (
     </div>
 
     <section className="main-video">
-      <section className="middle-text">
-        <h2>Never Forget Anything</h2>
-        <h3>This is a quote about memory.</h3>
-
-        <Link to="/login"><button className="landing-button">LOG IN</button></Link>
-        <p>or</p>
-        <Link to="/login"><button className="landing-button green">SIGN UP FOR FREE</button></Link>
-      </section>
+      <div className="video-frame" dataVideo="/evernote.com/video/homepage" dataImage="/evernote.com/img/homepage/homepage-hero-video-desktop-still.jpg" dataMobile="/evernote.com/img/homepage/homepage-hero-video-mobile-still.jpg">
+        <video autoPlay="autoplay" loop="loop" poster="https://cdn1.evernote.com/evernote.com/img/homepage/homepage-hero-video-desktop-still.jpg">
+          <source className="webm" type="video/webm" src="https://cdn1.evernote.com/evernote.com/video/homepage/homepage-hero-video.webm" />
+          <source className="mp4" type="video/mp4" src="https://cdn1.evernote.com/evernote.com/video/homepage/homepage-hero-video.mp4" />
+        </video>
+        <img src="https//cdn1.evernote.com/evernote.com/img/homepage/homepage-hero-video-desktop-still.jpg" className="mobile-fallback" alt />
+      </div>
     </section>
+
+    <section className="middle-text">
+      <h2>Never Forget Anything</h2>
+      <h3>This is a quote about memory.</h3>
+
+      <Link to="/login"><button className="landing-button white">LOG IN</button></Link>
+      <p>or</p>
+      <Link to="/login"><button className="landing-button green">SIGN UP FOR FREE</button></Link>
+    </section>
+
   </div>
 );
 
